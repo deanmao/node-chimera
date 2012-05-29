87b8a48179f1f5edb1249689b82d77f50542cdb0
Dean Mao <deanmao@gmail.com>
2012-05-28 22:30:15 -0700

    add gmail example

--- example.js
+++ example.js
@@ -1,34 +1,40 @@
 var Chimera = require('./lib/main');
+var fs = require('fs');
+
+var jquerySrc = fs.readFileSync("jquery.js");
 
 var c = new Chimera({
-  libraryCode: "window.whatever = \"zzzzzz\";",
-  cookies: "k=69.181.249.220.1338265858459430; expires=Tue, 05-Jun-2012 04:30:58 GMT; domain=.twitter.com; path=/guest_id=v1%3A133826585846559688; expires=Thu, 29-May-2014 04:30:58 GMT; domain=.twitter.com; path=/_mobile_sess=BAh7BzoQX2NzcmZfdG9rZW4iGTQ0ODc3YjQxMzVlZDY3OTg5NjEyOg9zZXNzaW9uX2lkIiViNjk4NGIwOTAyZTgyOGI4MTk0MjM4ZDJlYzM5ODNhZQ%3D%3D--6d0d51ad047eca2e2199c9663c79421c5dd58279; expires=Wed, 25-Jul-2012 00:52:36 GMT; domain=mobile.twitter.com; path=/"
+  libraryCode: jquerySrc
 });
 c.perform({
-  url: "http://mobile.twitter.com/deanmao",
+  url: "http://gmail.com",
   locals: {
-    yourName: "Johnson"
+    username: "blahblah",
+    password: "mypassword"
   },
   run: function(callback) {
-    console.log('inside browser 1: '+window.location);
-    console.log('your name: '+yourName);
-    console.log('whatever: '+window.whatever);
-    document.cookies = "asdf=1";
-    callback("", document.cookies);
+    jQuery.noConflict();
+    jQuery('#Email').val(username);
+    jQuery('#Passwd').val(password);
+    callback("", document.cookie);
+    jQuery('#signIn').click();
   },
   callback: function(err, result) {
-    console.log("got the result: " + result);
-    console.log("got the cookies: " + c.cookies())
+    console.log('==================================');
+    console.log("document.cookie: " + result);
     
     c.perform({
       run: function(callback) {
-        console.log('running without a url: ' + window.location);
-        console.log('cookies: ' + document.cookies);
-        callback(null, "whatever");
+        setTimeout(function() {
+          callback(null, document.cookie);
+        }, 1000);
       },
       callback: function(err, result) {
-        console.log("result without url is: " + result);
+        console.log('==================================');
+        console.log("final document.cookie: " + result);
+        console.log('----------------------------------');
         console.log("final cookies: " + c.cookies())
+        console.log('----------------------------------');
       }
     });
   }
