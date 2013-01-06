7a317cfc9f1b6069323f1073050af50fa32b95b3
Dean Mao <deanmao@gmail.com>
2013-01-06 15:07:52 -0800

    add s3 binary uploader

--- scripts/download_native_binary.js
+++ scripts/download_native_binary.js
@@ -3,4 +3,4 @@ var request = require('request');
 var zlib = require('zlib');
 var platform_arch = process.platform + "_" + process.arch;
 console.log("downloading chimera native binary for: "+platform_arch);
-request('https://github.com/deanmao/node-chimera/blob/master/native/'+platform_arch+'/chimera.node?raw=true').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('lib/chimera.node'));
+request('http://s3.chimerajs.com/chimera_'+platform_arch+'.gz').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('lib/chimera.node'));
---
+++ scripts/upload_natives.js
@@ -0,0 +1,29 @@
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+AWS.config.update({
+  region: 'us-east-1',
+  accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY
+});
+
+
+function upload(filename, key) {
+  console.log('uploading: '+filename);
+  var s3 = new AWS.S3()
+  s3.client.putObject({
+    ACL : 'public-read',
+    Bucket: "s3.chimerajs.com",
+    Key: key,
+    Body: fs.readFileSync(filename)
+  }, function(err, res) {
+    console.log(err, res);
+  });
+}
+
+try {
+  upload(__dirname + '/../native/darwin_x64/chimera.node.gz', 'chimera_darwin_x64.gz');
+  upload(__dirname + '/../native/linux_x64/chimera.node.gz', 'chimera_linux_x64.gz');
+} catch(e) {
+  console.log(e);
+}
