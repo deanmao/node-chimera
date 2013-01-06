22131ec50737d7b767dd95570c683f820b1f47ba
Dean Mao <deanmao@gmail.com>
2013-01-06 14:21:25 -0800

    make it download gzipped binaries

--- scripts/download_native_binary.js
+++ scripts/download_native_binary.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var request = require('request');
+var zlib = require('zlib');
 var platform_arch = process.platform + "_" + process.arch;
 console.log("downloading chimera native binary for: "+platform_arch);
-request('https://github.com/deanmao/node-chimera/blob/master/native/'+platform_arch+'/chimera.node?raw=true').pipe(fs.createWriteStream('lib/chimera.node'));
+request('https://github.com/deanmao/node-chimera/blob/master/native/'+platform_arch+'/chimera.node?raw=true').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('lib/chimera.node'));
