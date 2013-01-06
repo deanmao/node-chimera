f93bbef3fdc4e4b674cb7080a5d49df514111514
Dean Mao <deanmao@gmail.com>
2013-01-06 14:16:01 -0800

    downloader

--- scripts/download_native_binary.js
+++ scripts/download_native_binary.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 var request = require('request');
 var platform_arch = process.platform + "_" + process.arch;
-request('https://github.com/deanmao/node-chimera/blob/master/native/'+platform_arch+'/chimera.node?raw=true').pipe(fs.createWriteStream('lib/chimera.node'))
+console.log("downloading chimera native binary for: "+platform_arch);
+request('https://github.com/deanmao/node-chimera/blob/master/native/'+platform_arch+'/chimera.node?raw=true').pipe(fs.createWriteStream('lib/chimera.node'));
