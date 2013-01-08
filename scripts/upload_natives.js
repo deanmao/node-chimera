e51b94ffe8730a93f07549e55413cc84fc43549f
Dean Mao <deanmao@gmail.com>
2013-01-08 11:42:01 -0800

    tag binary with package version

--- scripts/upload_natives.js
+++ scripts/upload_natives.js
@@ -1,5 +1,7 @@
 var AWS = require('aws-sdk');
 var fs = require('fs');
+var info = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
+var version = info["version"];
 
 AWS.config.update({
   region: 'us-east-1',
@@ -7,9 +9,10 @@ AWS.config.update({
   secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY
 });
 
-
-function upload(filename, key) {
-  console.log('uploading: '+filename);
+function upload(platform_arch) {
+  var filename = __dirname + '/../native/'+platform_arch+'/chimera.node.gz';
+  var key = 'chimera_'+platform_arch+'__'+version+'.gz';
+  console.log('uploading: '+filename+' as '+key);
   var s3 = new AWS.S3()
   s3.client.putObject({
     ACL : 'public-read',
@@ -22,9 +25,9 @@ function upload(filename, key) {
 }
 
 try {
-  upload(__dirname + '/../native/darwin_x64/chimera.node.gz', 'chimera_darwin_x64.gz');
-  upload(__dirname + '/../native/linux_x64/chimera.node.gz', 'chimera_linux_x64.gz');
-  upload(__dirname + '/../native/linux_ia32/chimera.node.gz', 'chimera_linux_ia32.gz');
+  upload('darwin_x64');
+  upload('linux_x64');
+  upload('linux_ia32');
 } catch(e) {
   console.log(e);
 }
