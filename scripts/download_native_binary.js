73306453ecd10dd38695317223da79f523be83e8
Dean Mao <deanmao@DeanMao-MBP.local>
2013-06-26 12:17:45 -0700

    fix downloader

--- scripts/download_native_binary.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var request = require('request');
-var zlib = require('zlib');
-var platform_arch = process.platform + "_" + process.arch;
-var info = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
-var url = 'http://s3.chimerajs.com/chimera_'+platform_arch+'__'+info["version"]+'.gz';
-console.log('downloading binary for '+platform_arch+' with version '+info["version"]);
-console.log('full download url: ' + url);
-request(url).pipe(zlib.createGunzip()).pipe(fs.createWriteStream('lib/chimera.node'));
--- scripts/downloader.js
+++ scripts/downloader.js
@@ -1,28 +1,52 @@
 var spawn = require('child_process').spawn;
 var http = require('http');
 var fs = require('fs');
-var options = {
-  host: 's3.chimerajs.com',
-  port: 80,
-  path: '/qt5_'+process.platform+'_'+process.arch+'.tar.gz'
-};
 var cwd = __dirname+'/../';
 
-http.get(options, function(res) {
-  res.setEncoding('binary');
-  var fileData = '';
-  res.on("data", function(chunk) {
-    fileData += chunk;
+function untar(cb) {
+  spawn('tar', ['xf', 'qt.tar.gz'], {cwd: cwd}).on('close', function() {
+    cb();
   });
-  res.on("end", function() {
-    fs.writeFile(cwd+'qt.tar.gz', fileData, 'binary', function(err){
-        if (err) throw err
-        console.log('finished downloading, now extracting tar.gz file');
-        spawn('tar', ['zvxf', cwd+'qt.tar.gz'], {cwd: cwd}).on('close', function() {
-          console.log('done with extraction');
+}
+
+function download(cb) {
+  var options = {
+    host: 's3.chimerajs.com',
+    port: 80,
+    path: '/qt5_'+process.platform+'_'+process.arch+'.tar.gz'
+  };
+  http.get(options, function(res) {
+    res.setEncoding('binary');
+    var fileData = '';
+    res.on("data", function(chunk) {
+      fileData += chunk;
+    });
+    res.on("end", function() {
+      fs.writeFile(cwd+'qt.tar.gz', fileData, 'binary', function(err){
+          if (err) throw err
+          console.log('finished downloading, now extracting tar.gz file');
+          cb();
+       });
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+}
+
+fs.exists(cwd+'qt.tar.gz', function(exists) {
+  if (exists) {
+    fs.exists(cwd+'qt', function(exists) {
+      if (!exists) {
+        untar(function() {
+          console.log('done!');
         });
-     });
-  });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+      }
+    });
+  } else {
+    download(function() {
+      untar(function() {
+        console.log('done!');
+      });
+    });
+  }
 });
